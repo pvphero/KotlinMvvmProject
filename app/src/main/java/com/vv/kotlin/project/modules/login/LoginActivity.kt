9fa65cc7217afa886a0e4768f906a13f209753ae
pvphero <impvphero@163.com>
2020-10-30 11:45:00 +0800

    自动脚本

--- app/src/main/java/com/vv/kotlin/project/modules/login/LoginActivity.kt
+++ app/src/main/java/com/vv/kotlin/project/modules/login/LoginActivity.kt
@@ -1,7 +1,21 @@
 package com.vv.kotlin.project.modules.login
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.vv.kotlin.project.R
+
 /**
  *
  * @author ShenZhenWei
  * @date 2020/10/30
  */
+class LoginActivity :AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_user_login)
+
+
+    }
+}
--- app/src/main/java/com/vv/kotlin/project/net/APIClient.kt
+++ app/src/main/java/com/vv/kotlin/project/net/APIClient.kt
@@ -6,16 +6,16 @@ package com.vv.kotlin.project.net
  * @date 2020/10/30
  */
 class APIClient {
-    private object Holder{
-        val INSTANCE=APIClient();
+    private object Holder {
+        val INSTANCE = APIClient();
     }
 
-    companion object{
-        val instance=Holder.INSTANCE
+    companion object {
+        val instance = Holder.INSTANCE
     }
 
     //WanAndroid API实例化
-    fun<T> instanceRetrofit(apiInterface:Class<T>):T{
+    fun <T> instanceRetrofit(apiInterface: Class<T>): T {
 
     }
 }
