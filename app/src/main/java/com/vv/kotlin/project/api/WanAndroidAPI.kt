9aa79a6b3f11747533aa9baab90282afe2582044
pvphero <impvphero@163.com>
2020-10-30 11:15:00 +0800

    自动脚本

---
+++ app/src/main/java/com/vv/kotlin/project/api/WanAndroidAPI.kt
@@ -0,0 +1,16 @@
+package com.vv.kotlin.project.api
+
+/**
+ *
+ * @author ShenZhenWei
+ * @date 2020/10/30
+ *
+ * 客户端API 可以访问 服务器API
+ */
+interface WanAndroidAPI {
+    /**
+     * 登陆API
+     *
+     * 
+     */
+}
